cc3cfbcaab9d2b160b1a5ecdacb10a74d914b046
Willie Roberts <diagoza@me.com>
2022-11-13 11:07:01 +0200

    Add project files.

---
+++ Cyber.Mods.UI/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Cyber.Mods.UI;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Cyber.Mods.UI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Cyber.Mods.UI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
